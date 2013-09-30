c43835e11aa821c5b1de18c8bc96d9abc8e76885
Ian Shurmer <ian@hostitlocal.co.uk>
2013-09-30 16:54:24 +0100

    Add base commit of serializer for REST framework.

--- eav/validators.py
+++ eav/validators.py
@@ -109,6 +109,5 @@ def validate_enum(value):
         raise ValidationError(_(u"EnumValue has not been saved yet"))
 
 def validate_distance(value):
-    return
     from measurement import validators
     validators.valid_unit_type(value)
